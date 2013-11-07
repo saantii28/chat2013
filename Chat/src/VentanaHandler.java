29ad38dfe72b262d0854aee067550d72f4e549bf
Santiago Perez Torre <santiago.pereztorre@gmail.com>
2013-11-06 23:56:59 -0300

    Modify VentanaHandler
    Agregado el método getContactos()

--- Chat/src/VentanaHandler.java
+++ Chat/src/VentanaHandler.java
@@ -1,4 +1,8 @@
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /*
@@ -13,8 +17,26 @@ import java.util.ArrayList;
  */
 public class VentanaHandler {
     
-    public ArrayList<Contactos> getContacts(){
-        return null;
+    public static ArrayList<Contacto> getContactos() throws SQLException{
+        ArrayList<Contacto> contactos = new ArrayList();
+        Connection c = null;
+        PreparedStatement stmt = null;
+        try {
+            c = DBHandler.getConnection();
+            stmt = c.prepareStatement("Select * from contactos");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Contacto co = new Contacto();
+                co.setId(rs.getString("co_id"));
+                co.setName(rs.getString("co_name"));
+                co.setMail(rs.getString("co_mail"));
+                contactos.add(co);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+        return contactos;
     }
     
 }
