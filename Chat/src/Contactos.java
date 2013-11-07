711b387774b7a5466a6469ca3f635c6d42c45e1c
Santiago Perez Torre <santiago.pereztorre@gmail.com>
2013-11-06 23:43:24 -0300

    Add Contactos Class
    Define un contactos como (id, name, mail)

---
+++ Chat/src/Contactos.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Santiago-Laptop
+ */
+class Contactos {
+    
+    private String id;
+    private String name;
+    private String mail;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the mail
+     */
+    public String getMail() {
+        return mail;
+    }
+
+    /**
+     * @param mail the mail to set
+     */
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
