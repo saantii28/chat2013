187a9b93bbde2c18587123da5836c7fe4a73dd8d
Santiago Perez Torre <santiago.pereztorre@gmail.com>
2013-11-08 23:43:05 -0300

    Added Mensaje Class
    Formato (id_de, id_ha, cuerpo, nombre_de, nombre_ha, fecha)
    Utilidad: manejar los mensajes enviados y recibidos

---
+++ Chat/src/Mensaje.java
@@ -0,0 +1,108 @@
+
+
+
+import java.sql.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Santiago-Laptop
+ */
+class Mensaje {
+    
+    private String id_de;
+    private String id_ha;
+    private String cuerpo;
+    private String nombre_de;
+    private String nombre_ha;
+    private Date fecha;
+
+    /**
+     * @return the id_de
+     */
+    public String getId_de() {
+        return id_de;
+    }
+
+    /**
+     * @param id_de the co_id to set
+     */
+    public void setId_de(String co_id) {
+        this.id_de = co_id;
+    }
+
+    /**
+     * @return the id_ha
+     */
+    public String getId_ha() {
+        return id_ha;
+    }
+
+    /**
+     * @param id_ha the co_ha to set
+     */
+    public void setId_ha(String co_ha) {
+        this.id_ha = co_ha;
+    }
+
+    /**
+     * @return the cuerpo
+     */
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    /**
+     * @param cuerpo the cuerpo to set
+     */
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    /**
+     * @return the nombre_de
+     */
+    public String getNombre_de() {
+        return nombre_de;
+    }
+
+    /**
+     * @param nombre_de the nombre_de to set
+     */
+    public void setNombre_de(String nombre_de) {
+        this.nombre_de = nombre_de;
+    }
+
+    /**
+     * @return the nombre_ha
+     */
+    public String getNombre_ha() {
+        return nombre_ha;
+    }
+
+    /**
+     * @param nombre_ha the nombre_ha to set
+     */
+    public void setNombre_ha(String nombre_ha) {
+        this.nombre_ha = nombre_ha;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}
